1bb301e4d014a167ea30dbe7e25a0ce98b536643
tsubasa <49914377+Tsubasa1001@users.noreply.github.com>
2021-02-11 10:14:14 -0400

    Add files via upload

--- src/comando/Separar_Comando.java
+++ src/comando/Separar_Comando.java
@@ -8,20 +8,26 @@ package comando;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import Datos.DPaciente;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author sariah
  */
 public class Separar_Comando {
     
+    DPaciente paciente;
+    String cuActual;
+    String accionActual;
     String comando;
     
-    String[]  listaCU = {"USUARIO","SERVICIO","EQUIPAMIENTO","AGENDA","FICHAMEDICA"};
+    String[]  listaCU = {"USUARIO-TRABAJADOR","USUARIO-PACIENTE","SERVICIO","EQUIPAMIENTO","AGENDA","FICHAMEDICA"};
     String[] lista_acciones = {"LISTAR","REGISTRAR","MODIFICAR","ELIMINAR"};
     
     
     public Separar_Comando(){
-          
+          paciente = new DPaciente();
     }
     public Separar_Comando(String comando){
         this.comando = comando;    
@@ -35,7 +41,7 @@ public class Separar_Comando {
         String parametros = separado[2];
         
         //pruebas para la separacion de parametros
-        String[] parametrosSeparados = parametros.split(",");
+        /*String[] parametrosSeparados = parametros.split(",");
         String parametro1 = parametrosSeparados[0];
         String parametro2 = parametrosSeparados[1];
         String parametro3 = parametrosSeparados[2];
@@ -44,47 +50,66 @@ public class Separar_Comando {
         //System.err.println("Accion: "+accion);
         System.err.println("Parametros: "+parametros);
         System.err.println("Parametros separados: "+parametro1+" "+parametro2+" "+parametro3);
-        
+       */ 
         for (int i=0; i<listaCU.length; i++){
             if(listaCU[i].equals(cu)){
+                cuActual = listaCU[i];
                 System.out.println("CU encontrado en la lista: "+ listaCU[i]);
+          
             }else{
                 //System.err.println("CU no encontrado en la lista");
             }
+            
         }
+        
+        
         
         for (int i=0; i<lista_acciones.length; i++){     
             if(lista_acciones[i].equals(accion)){
+                accionActual=lista_acciones[i];
                 System.out.println("Accion encontrada en la lista: "+ lista_acciones[i]);
                 //llamar al negocio correspondiente y mandarle el parametro accion con sus atributos
             }else{
                 //System.err.println("Accion no encontrada en la lista: "+ lista_acciones[i]);
             }
         }
-          
-           
-    }
-    
-    public String encontrarCU(String comando){
         
-        
-        String cu_encontrado;
-        cu_encontrado = " ";
-        return cu_encontrado;
+        switch(cuActual){
+            case "USUARIO-PACIENTE":
+                System.err.println("paso por USUARIO-PACIENTE");
+                switch(accionActual){
+                    case "LISTAR":
+                        System.err.println("paso por LISTAR");
+                        System.out.println(paciente.Dlistar());
+                        break;
+                    case "REGISTRAR":
+                        paciente.Dregistrar(comando, cu, comando, accionActual, accion, accion, accion, cu, 0);
+                        break;
+                    case "MODIFICAR":
+                        paciente.Dmodificar(comando, cu, comando, accionActual, accion, accion, accion, cu, 0);
+                        break;
+                    case "ELIMINAR":
+                        paciente.Deliminar(comando);
+                        break;
+                }            
+        }      
     }
     
-    public String encontrarAccion(String comando){
-        String accion_encontrada;
-        accion_encontrada = " ";
-        return accion_encontrada;
     
-    }
+    
+
     
     public static void main(String[] args){
-        
-       Separar_Comando separar_comando = new Separar_Comando();
-       separar_comando.separacion("SERVICIO_REGISTRAR_nombre,apellido,correo");
+       DPaciente paciente = new DPaciente();
        
+       
+        //System.out.println(paciente.Dlistar());
+        //System.out.println(paciente.Dregistrar("P0003","42123", "Gustavo", "Bolivia", "Ing", "Por alla", "gustav@gmail.com", "75698542", 20));
+        //System.out.println(paciente.Dmodificar("P003","00033", "Gustavo", "Bolivia", "Ing.", "Por aquiii", "gustav@gmail.com", "75698542", 23));
+        //System.out.println(paciente.Deliminar("P003"));
+        Separar_Comando separar_comando = new Separar_Comando();
+        separar_comando.separacion("USUARIO-PACIENTE_LISTAR_{BULL}");
+       //System.out.println(paciente.Dlistar());
         
     }
     
