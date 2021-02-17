828e78d6ad12ad647f706703895460a5e0ac0ed9
tsubasa <49914377+Tsubasa1001@users.noreply.github.com>
2021-02-17 18:06:57 -0400

    Update creado_en_Servidor.sql
    Agregando y quitando algunas referencias a las tablas "consulta" y "cita consulta"
    Modificando tipo de dato "char" a "varchar" en las tablas "paciente" y "trabajador"

--- script/creado_en_Servidor.sql
+++ script/creado_en_Servidor.sql
@@ -19,16 +19,16 @@ select * from paquete
 
 CREATE TABLE trabajador(
 	id SERIAL primary key,
-	codigo char(15),
-	ci char(10) NOT NULL,
-	nombre char(150) NOT NULL,
-	nacionalidad char(100) NOT NULL,
-	especialidad char(50) NOT NULL,
-	cargo char(50) NOT NULL,
-	ocupacion char(50) NOT NULL,
-	direccion char(50) NOT NULL,
-	email char(50) UNIQUE NOT NULL,
-	celular char(8) NOT NULL,
+	codigo varchar(15),
+	ci varchar(10) NOT NULL,
+	nombre varchar(150) NOT NULL,
+	nacionalidad varchar(100) NOT NULL,
+	especialidad varchar(50) NOT NULL,
+	cargo varchar(50) NOT NULL,
+	ocupacion varchar(50) NOT NULL,
+	direccion varchar(50) NOT NULL,
+	email varchar(50) UNIQUE NOT NULL,
+	celular varchar(8) NOT NULL,
 	edad int NOT NULL,
 	genero char(1) NOT NULL
 );
@@ -38,12 +38,7 @@ drop table if exists trabajador;
 
 #insertar datos
 INSERT INTO trabajador VALUES (default,'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 1, 'M');
-INSERT INTO trabajador VALUES ('b', 'b', 'a', 'a', 'a', 'a', 'a', 'a', 'b', 'a', 1, 'F');
-INSERT INTO trabajador VALUES ('c', 'c', 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'a', 1, 'M');
-INSERT INTO trabajador VALUES ('d', 'd', 'a', 'a', 'a', 'a', 'a', 'a', 'd', 'a', 1, 'F');
-INSERT INTO trabajador VALUES ('e', 'e', 'a', 'a', 'a', 'a', 'a', 'a', 'e', 'a', 1, 'M');
-
-
+INSERT INTO trabajador VALUES (default,'b', 'b', 'a', 'a', 'a', 'a', 'a', 'a', 'b', 'a', 1, 'F');
 
 #consulta
 select * from trabajador;
@@ -51,14 +46,14 @@ select * from trabajador;
 
 CREATE TABLE paciente(
 	id SERIAL primary key,
-	codigo char(15),
-	ci char(10) NOT NULL,
-	nombre char(150) NOT NULL,
-	nacionalidad char(100) NOT NULL,
-	ocupacion char(50) NOT NULL,
-	direccion char(50) NOT NULL,
-	email char(50) UNIQUE NOT NULL,
-	celular char(8) NOT NULL,
+	codigo varchar(15),
+	ci varchar(10) NOT NULL,
+	nombre varchar(150) NOT NULL,
+	nacionalidad varchar(100) NOT NULL,
+	ocupacion varchar(50) NOT NULL,
+	direccion varchar(50) NOT NULL,
+	email varchar(50) UNIQUE NOT NULL,
+	celular varchar(8) NOT NULL,
 	edad int NOT NULL,
 	genero char(1) NOT NULL
 );
@@ -69,10 +64,8 @@ drop table if exists paciente;
 
 #insertar datos
 INSERT INTO paciente VALUES ( default,'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 1, 'M');
-INSERT INTO paciente VALUES ('b', 'b', 'a', 'a', 'a', 'a', 'b', 'a', 1, 'F');
-INSERT INTO paciente VALUES ('c', 'c', 'a', 'a', 'a', 'a', 'c', 'a', 1, 'M');
-INSERT INTO paciente VALUES ('d', 'd', 'a', 'a', 'a', 'a', 'd', 'a', 1, 'F');
-INSERT INTO paciente VALUES ('e', 'e', 'a', 'a', 'a', 'a', 'e', 'a', 1, 'M');
+INSERT INTO paciente VALUES (default,'b', 'b', 'a', 'a', 'a', 'a', 'b', 'a', 1, 'F');
+
 
 select * from paciente
 
@@ -129,36 +122,6 @@ INSERT into servicio values(default, 'Servicio1', 50.0, 1,1);
 #consulta
 select * from servicio
 
-#crear tabla
-
-create table consulta(
-	id SERIAL,
-	id_paciente integer,
-	id_servicio integer,
-	codigo varchar(50),
-	horaEntrada time,
-	horaSalida time,
-	fecha date,
-	precio real,
-	notas varchar(255),
-	diagnosticoFinal varchar(255),
-	primary key(id,id_paciente,id_servicio),
-	foreign key (id_paciente)references paciente(id),
-	foreign key (id_servicio) references servicio(id)
-	ON UPDATE CASCADE
-);
-
-#eliminar tabla
-drop table if exists consulta;
-
-#insertar datos
-INSERT INTO consulta VALUES (default, 1, 1, 'CON001', '04:00:00', '04:20:00', '2001-10-05', 200.0, 'ninguno', 'No se presentan anomalias');
-
-
-
-#consulta
-select * from consulta
-
 create table citaConsulta(
 	id SERIAL,
 	id_paciente integer,
@@ -169,7 +132,7 @@ create table citaConsulta(
 	motivoConsulta varchar(255)NOT NULL,
 	estadoTratamiento varchar(255)NOT NULL,
 	
-	primary key (id,id_paciente,id_trabajador),
+	primary key (id),
 	foreign key (id_paciente)references paciente(id),
 	foreign key (id_trabajador) references trabajador(id)
 	ON UPDATE CASCADE
@@ -181,7 +144,34 @@ drop table if exists citaConsulta;
 #insertar datos
 INSERT INTO citaConsulta values (default,1,1,'FC0001','02:30:00','2021-08-15','dolor de espalda','iniciando');
 
-
-
 #consulta
 select * from citaConsulta;
+
+#crear tabla
+
+create table consulta(  
+	id SERIAL,
+	id_citaConsulta integer,
+	id_servicio integer,
+	codigo varchar(50),
+	horaEntrada time,
+	horaSalida time,
+	fecha date,
+	precio real,
+	notas varchar(255),
+	diagnosticoFinal varchar(255),
+	primary key(id,id_citaConsulta,id_servicio),
+	foreign key (id_citaConsulta)references citaConsulta(id),
+	foreign key (id_servicio) references servicio(id)
+	ON UPDATE CASCADE
+);
+
+#eliminar tabla
+drop table if exists consulta;
+
+#insertar datos
+INSERT INTO consulta VALUES (default, 1, 1, 'CON001', '04:00:00', '04:20:00', '2001-10-05', 200.0, 'ninguno', 'No se presentan anomalias');
+
+#consulta
+select * from consulta
+
