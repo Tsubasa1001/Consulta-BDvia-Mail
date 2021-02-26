34eb3f6b0c4308e3ee52e59620c83648bc3dc4ff
CinisArboris <eyver.evm@gmail.com>
2021-02-25 21:36:26 -0400

    feat : función estadística [media]

--- src/Datos/DatosEstadistica.java
+++ src/Datos/DatosEstadistica.java
@@ -5,10 +5,59 @@
  */
 package Datos;
 
+import Consultas_Mail.ClientePgSql;
+
 /**
  *
  * @author eyver-dev
  */
-public class Estadistica {
+public class DatosEstadistica {
+    private ClientePgSql postgres;
+
+    public DatosEstadistica() {
+        postgres = new ClientePgSql();
+    }
+
+    public ClientePgSql getPostgres() {return postgres;}
+    public void setPostgres(ClientePgSql postgres) {this.postgres = postgres;}
     
+    /*V A R I A B L E S   A T E N C I O N*/
+    
+    public int cantTotalAtenciones() {
+        int resultado = -1;
+        String sql = "default";
+        resultado = (int) (Math.random()*1000);
+        
+        sql = "select count(*) from usuario";
+        this.getPostgres().connect();
+        String trash = this.getPostgres().runStatement(sql);
+        trash = trash.replace("|", "");
+        trash = trash.replace(",", "");
+        trash = trash.substring(0, trash.length() -2);
+        resultado = Integer.parseInt(trash);
+        this.getPostgres().desconectar();
+        
+        return resultado;
+    }
+    public int sumTotalAtenciones() {
+        int resultado = -1;
+        String sql = "default";
+        resultado = (int) (Math.random()*1000+1000);
+        
+        sql = "select sum(codigo) from usuario";
+        this.getPostgres().connect();
+        String trash = this.getPostgres().runStatement(sql);
+        trash = trash.replace("|", "");
+        trash = trash.replace(",", "");
+        trash = trash.substring(0, trash.length() -2);
+        resultado = Integer.parseInt(trash);
+        this.getPostgres().desconectar();
+        
+        return resultado;
+    }
+    public int cantTotalAtencionesMes() {
+        int resultado = -1;
+        resultado = cantTotalAtenciones()/12;
+        return resultado;
+    }
 }
--- src/negocio/NegocioEstadistica.java
+++ src/negocio/NegocioEstadistica.java
@@ -5,10 +5,74 @@
  */
 package negocio;
 
+import Datos.DatosEstadistica;
+
 /**
  *
  * @author eyver-dev
  */
-public class Estadistica {
+public class NegocioEstadistica {
+    private DatosEstadistica datosEstadistica;
+    private String white = "\u001B[37m";
+    private String cyan = "\u001B[36m";
+    private String purple = "\u001B[35m";
+
+    public NegocioEstadistica() {
+        datosEstadistica = new DatosEstadistica();
+    }
+
+    public DatosEstadistica getDatosEstadistica() {return datosEstadistica;}
+    public void setDatosEstadistica(DatosEstadistica datosEstadistica) {this.datosEstadistica = datosEstadistica;}
+    
+    /*M E T O D O S   E S T A D I S T I C O S*/
+    
+    private String mediaAtencionTotal() {
+        String resultado = "default";
+        int cantTotalAtenciones = -1;
+        int sumTotalAtenciones = -1;
+        int mediaTotalAtenciones = -1;
+        
+        cantTotalAtenciones = this.getDatosEstadistica().cantTotalAtenciones();
+        sumTotalAtenciones = this.getDatosEstadistica().sumTotalAtenciones();
+        try {
+            mediaTotalAtenciones = sumTotalAtenciones/cantTotalAtenciones;
+        }catch(Exception e){
+            mediaTotalAtenciones = 0;
+        }
+        
+        resultado = this.cyan + "mediaAtencionTotal :: " + mediaTotalAtenciones + this.white;
+        return resultado;
+    }
+    private String mediaAtencionMes() {
+        String resultado = "default";
+        int cantTotalAtencionesMes = -1;
+        int sumTotalAtenciones = -1;
+        int mediaAtencionMes = -1;
+        
+        cantTotalAtencionesMes = this.getDatosEstadistica().cantTotalAtencionesMes();
+        sumTotalAtenciones = this.getDatosEstadistica().sumTotalAtenciones();
+        try{
+            mediaAtencionMes = sumTotalAtenciones/cantTotalAtencionesMes;
+        }catch(Exception e){
+            mediaAtencionMes = 0;
+        }
+        
+        resultado = this.purple + "mediaAtencionMes :: " + mediaAtencionMes + this.white;
+        return resultado;
+    }
+    
+    public static void main(String[] args) {
+        NegocioEstadistica ne = new NegocioEstadistica();
+        String tmp = "default";
+        
+        tmp = ne.mediaAtencionTotal();
+        System.out.println(tmp);
+        
+        tmp = ne.mediaAtencionMes();
+        System.out.println(tmp);
+    }
+
+    
+
     
 }
