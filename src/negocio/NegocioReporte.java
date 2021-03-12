99ca927e63412cd7f41e4ecb574a34f72bb0997d
CinisArboris <eyver.evm@gmail.com>
2021-03-11 21:06:23 -0400

    fix : lectura del mensaje = 0 (indice)

--- src/negocio/NegocioReporte.java
+++ src/negocio/NegocioReporte.java
@@ -39,7 +39,7 @@ public class NegocioReporte {
          * Banner
          */
         
-        for (int i = 1; i < tmp.length; i++){
+        for (int i = 0; i < tmp.length; i++){
             String[] aux = tmp[i].split(",");
             for (int j = 0; j < aux.length; j++){
                 aux[j] = aux[j].trim();
